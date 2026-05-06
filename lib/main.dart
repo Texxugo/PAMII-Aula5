535c87be8b00a18b2f077fa3da320e5810e65294
LUIS FELIPE DOS SANTOS FERREIRA <176322025@ETEC.BR>
2026-05-06 20:18:40 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'screens/usuarios_screen.dart';
+
+void main() {
+  runApp(const MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aula 08 - API REST',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        useMaterial3: true,
+      ),
+      home: const UsuariosScreen(),
+    );
+  }
+}
